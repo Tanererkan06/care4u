e8f471c27362aa2ca604038bf18e159f6d7f3027
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2023-07-16 18:33:26 +0300

    dd
    dd

--- care4u/app/_layout.tsx
+++ care4u/app/_layout.tsx
@@ -4,7 +4,8 @@ import { useFonts } from 'expo-font';
 import { SplashScreen, Stack } from 'expo-router';
 import { useEffect } from 'react';
 import { useColorScheme } from 'react-native';
-
+//git rm -r --cached .
+// git temizle
 export {
   // Catch any errors thrown by the Layout component.
   ErrorBoundary,
