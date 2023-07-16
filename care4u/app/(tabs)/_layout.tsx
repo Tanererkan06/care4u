eff72e3b311c031164e87be5510506407f9b9023
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2023-07-17 00:21:40 +0300

    dd
    dd

--- care4u/app/(tabs)/_layout.tsx
+++ care4u/app/(tabs)/_layout.tsx
@@ -1,7 +1,6 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { Link, Tabs } from 'expo-router';
-import { Pressable, useColorScheme } from 'react-native';
-
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Pressable, useColorScheme } from 'react-native';
 import Colors from '../../constants/Colors';
 
 /**
@@ -14,57 +13,120 @@ function TabBarIcon(props: {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
 }
 
+
+ 
+
+
+
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const colorScheme = useColorScheme(); 
+  const styles = StyleSheet.create({
+    image: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: 'center',
+    },
+  
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+  
+    },
+    logo: {
+      fontWeight: "bold",
+      fontSize: 50,
+      color: "lightblue",
+      marginBottom: 40
+    },
+    inputView: {
+      width: "80%",
+      backgroundColor: "lightblue",
+      borderRadius: 10,
+      height: 50,
+      marginBottom: 20,
+      justifyContent: "center",
+      padding: 20,
+    },
+    inputText: {
+      height: 50,
+      color: "white"
+    },
+    forgot: {
+      color: "black",
+      fontSize: 11
+    },
+    loginBtn: {
+      width: "80%",
+      backgroundColor: "#5DA7DB",
+      borderRadius: 25,
+      height: 50,
+      alignItems: "center",
+      justifyContent: "center",
+      marginTop: 40,
+      marginBottom: 10
+    },
+    loginText: {
+      color: "white"
+    }
+  });
 
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'SingIn',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="two"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
+    <View style={styles.container}>
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        }}>
         <Tabs.Screen
-        name="three"
-        options={{
-          title: 'Register',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-          <Tabs.Screen
-        name="four"
-        options={{
-          title: 'Product',
-          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-        }}
-      />
-      
-    </Tabs>
+          name="index"
+          options={{
+            title: 'SingIn',
+            headerShown: false,tabBarShowLabel:false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+            headerRight: () => (
+              <Link href="/modal" asChild>
+                <Pressable>
+                  {({ pressed }) => (
+                    <FontAwesome
+                      name="info-circle"
+                      size={25}
+                      color={Colors[colorScheme ?? 'light'].text}
+                      style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                    />
+                  )}
+                </Pressable>
+              </Link>
+            ),
+          }}
+        />
+        <Tabs.Screen
+          name="two"
+          options={{
+            title: 'Settings',
+            headerShown: false,tabBarShowLabel:false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="pencil" color={ color ? 'lightblue' : '#748c94'} />,
+          }}
+        />
+        <Tabs.Screen
+          name="three"
+          options={{
+            title: 'Register',
+            headerShown: false,tabBarShowLabel:false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="cog" color={ color ? 'lightblue' : '#748c94'}  />,
+          }}
+        />
+        <Tabs.Screen
+          name="four"
+          options={{
+            title: 'Product',
+            headerShown: false,tabBarShowLabel:false,
+            tabBarIcon: ({ color }) => <TabBarIcon name="file" color={ color ? 'lightblue' : '#748c94'}  />,
+          }}
+        />
+
+      </Tabs>
+    </View>
+
   );
 }
+
+
