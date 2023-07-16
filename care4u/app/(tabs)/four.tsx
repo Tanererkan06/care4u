d0fed87f8907fcc677336d0f4fc700ffe3c02f01
Taner ERKAN <Tanererkan06@users.noreply.github.com>
2023-07-17 00:49:10 +0300

    e
    e

--- care4u/app/(tabs)/four.tsx
+++ care4u/app/(tabs)/four.tsx
@@ -1,14 +1,18 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TextInput, TouchableOpacity, ImageBackground, Pressable, useColorScheme } from 'react-native';
+
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
-export default function TabThreeScreen() {
+export default function TabFourScreen() {
   return (
+
     <View style={styles.container}>
-      <Text style={styles.title}>Three</Text>
+      <ImageBackground source={require('../../images/background.jpg')} style={styles.image} >
+      <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <EditScreenInfo path="app/(tabs)/index.tsx" />
+      </ImageBackground> 
     </View>
   );
 }
@@ -19,7 +23,56 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
+  image: {
+    flex: 1,
+    flexDirection: 'column',
+    resizeMode: 'cover',
+    justifyContent: "center",
+    alignItems: 'center',
+  }, 
+  text: {
+    color: 'white',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    backgroundColor: '#000000a0',
+  },
+  logo: {
+    fontWeight: "bold",
+    fontSize: 50,
+    color: "lightblue",
+    marginBottom: 40
+  },
+  inputView: {
+    width: "80%",
+    backgroundColor: "lightblue",
+    borderRadius: 10,
+    height: 50,
+    marginBottom: 20,
+    justifyContent: "center",
+    padding: 20,
+  },
+  inputText: {
+    height: 50,
+    color: "white"
+  },
+  forgot: {
+    color: "black",
+    fontSize: 11
+  },
+  loginBtn: {
+    width: "80%",
+    backgroundColor: "#5DA7DB",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    marginBottom: 10
+  },
+  loginText: {
+    color: "white"
+  },  title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
